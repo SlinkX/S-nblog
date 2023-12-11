a73c2b7494c8ab8fccdf58f6ab199ddc60f3bf6d
Xlink <3095421243@qq.com>
2023-12-11 19:41:37 +0800

    this debug

--- model/model.js
+++ model/model.js
@@ -1,45 +1,57 @@
 const mysql = require('mysql')
 
-// 数据模型的封装
-// 封装了数据库的操作
+/**
+ * 数据模型的基类
+ * 封装了数据库操作
+ */
+module.exports = class Model {
+    // 连接对象
+    static conn = null
 
-module.exports = class Model{
-  // 连接对象
-  static conn = null;
-
-  // 数据库的连接方法
-  static  connection(){
-    Model.conn = mysql.createConnection({
-      host:'127.0.0.1',
-      user:'root',
-      password:'1111',
-      database: 'blog'
-    })
-    Model.conn.connect(err=>{
-      if(err){
-        console.log(`数据库连接失败：${err.message}`)
-      }
-    })
-  }
-
-  // 关闭数据库
-  static end(){
-    if(null != Model.conn){
-      Model.conn.end();
+    /**
+     * 数据库连接方法
+     */
+    static connection() {
+        Model.conn = mysql.createConnection({
+            host: '127.0.0.1',
+            user: 'root',
+            password: 'ok',
+            database: 'blog'
+        })
+        Model.conn.connect(err => {
+            if (err) {
+                console.log(`数据库连接失败：${err.message}`)
+            }
+        })
     }
-  }
-  static query(sql,params=[]){
-    return new Promise((resolve, reject)=> {
-      this.connection()
 
-      Modle.conn.query(sql,params,(err,results)=>{
-        if(err){
-          reject(err)
-        }else{
-          resolve(results)
+    /**
+     * 关闭数据库连接
+     */
+    static end() {
+        if (null != Model.conn) {
+            Model.conn.end()
         }
-      })
-      this.end()
-    })
-  }
+    }
+
+    /**
+     * 通用查询方法
+     * @param {string} sql 要执行的SQL语句
+     * @param {Array} params 给SQL语句的占位符进行赋值的参数数组
+     */
+    static query(sql, params = []) {
+        return new Promise((resolve, reject) => {
+            this.connection()
+
+            Model.conn.query(sql,params,(err,results)=>{
+                if(err){
+                    reject(err)
+                }else{
+                    resolve(results)
+                }
+            })
+
+            this.end()
+        })
+    }
 }
